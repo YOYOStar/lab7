efb6b86a1930ea9f7693a1bd0b847559e23e366d
YOYOStar <3607779143@qq.com>
2025-03-30 17:12:44 +0800

    code for lab7

---
+++ src/test/java/org/example/LibraryTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class LibraryTest {
+
+    @Test
+    void mySort() {
+        int[] input = new int[] {2, 3, 1, 4, 5};
+        int[] expected = new int[] {1, 2, 3, 4, 5};
+        int[] actual = Library.mySort(input);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void detectEvent1() {
+        int input1 = 1;
+        boolean expect1 = true;
+        boolean actual = Library.detectOdd(input1);
+        assertEquals(expect1, actual);
+    }
+
+    @Test
+    void detectEvent2() {
+        int input2 = 2;
+        boolean expect2 = false;
+        boolean actual = Library.detectOdd(input2);
+        assertEquals(expect2, actual);
+    }
+
+}
